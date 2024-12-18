09df7ec30364f4082ddd5a4c7376ce5dc6b213e2
davi <davifilho.br@gmail.com>
2024-12-18 14:51:49 -0300

    Some changes in project

---
+++ Projeto/project/accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+def login_view(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        
+        user = authenticate(request, username=username, password="Fdelma4868733")
+
+        if user is not None:
+            login(request, user=user)
+            return redirect('post_list')
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+
+    return render(request, 'login.html', {'login_form': login_form})
--- Projeto/project/project/urls.py
+++ Projeto/project/project/urls.py
@@ -16,19 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from postSite.views import post_view, post_list, new_post_view, post_login, confirm_delete, confirm_edit, delete_post, edit_post
+from accounts.views import login_view
+from postSite.views import post_view, new_post_view, delete_post_view, edit_post_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', post_view),
-    path('loginPage/', post_view, name='loginPage'),
-    path('login/', post_login, name='login'),
     path('admin/', admin.site.urls),
-    path('posts/<str:user>', post_list, name="post_list"),
+    path('login/', login_view, name='login'),
+    path('posts/', post_view, name='post_list'),
     path('new_post/', new_post_view, name='new_post'),
-    path('confirm_del/<int:pk>', confirm_delete, name='confirm_del'),
-    path('confirm_edit/<int:pk>', confirm_edit, name='confirm_edit'),
-    path('delete/<int:pk>', delete_post, name='delete_post'),
-    path('edit/<int:pk>', edit_post, name='edit_post')
+    path('delete_post/<int:pk>', delete_post_view, name='delete_post'),
+    path('edit_post/<int:pk>', edit_post_view, name='edit_post'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
